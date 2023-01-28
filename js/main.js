f34b817f846b9777df276fe79e30fb4cbe9c2bd7
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-29 00:33:12 +0100

    add modal open and close feature

---
+++ js/main.js
@@ -0,0 +1,31 @@
+import Game from "./classes/Game.js";
+
+const game = new Game();
+
+//modal
+const openModalButton = document.getElementById("rules-btn");
+const closeModalButton = document.getElementById("close-btn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+function openModal() {
+	if (modal == null) {
+		return;
+	}
+
+	modal.classList.add("active");
+	overlay.classList.add("active");
+}
+
+function closeModal() {
+	if (modal == null) {
+		return;
+	}
+
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+}
+
+openModalButton.addEventListener("click", openModal);
+closeModalButton.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
