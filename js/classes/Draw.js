973aa23adef9171e0b5ec2ead157e33feaad5f89
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-01-30 00:58:47 +0100

    add Draw class and function to draw colors

---
+++ js/classes/Draw.js
@@ -0,0 +1,18 @@
+export default class Draw {
+	constructor() {
+		this.color_options = ["#F00", "#0F0", "#00F"];
+		let _result = this.drawResult();
+		this.getDrawResult = () => _result;
+	}
+
+	drawResult() {
+		const draw_colors = [];
+
+		for (let i = 0; i < this.color_options.length; i++) {
+			const index = Math.floor(Math.random() + this.color_options.length);
+			const draw_color = this.color_options[index];
+			draw_colors.push(draw_color);
+		}
+		return draw_colors;
+	}
+}
